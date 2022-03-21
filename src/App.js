1015930339e35ec3dcb259f487ef9cb8fe2a7468
Jack Schedel <jackschedel@gmail.com>
2022-03-21 15:47:48 -0400

    initial app create

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Form>
+          <Form.Group className="mb-3">
+            <Form.Label>Get ticker data</Form.Label>
+            <Form.Control type="name" placeholder="Enter stock ticker" />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Get Data
+          </Button>
+        </Form>
       </header>
     </div>
   );

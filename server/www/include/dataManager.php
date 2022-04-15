289b99759c2965be0a9476cb425edacb21cbeb83
Jack Schedel <jackschedel@gmail.com>
2022-04-15 14:23:12 -0400

    php done?

--- server/www/include/dataManager.php
+++ server/www/include/dataManager.php
@@ -1,4 +1,6 @@
 <?php
+	header("Access-Control-Allow-Origin: http://localhost:3000");
+	
 	
 	/*
 	
@@ -77,6 +79,9 @@ function tupleCount() {
 }
 
 function getStockData($ticker, $statistic, $resolution, $startDate, $endDate) {
+	
+	$i = 0;
+	
 	global $conn;
 	
 	if($resolution < 2) {
@@ -89,7 +94,8 @@ function getStockData($ticker, $statistic, $resolution, $startDate, $endDate) {
 	$statistic = strtolower($statistic);
 	
 	switch ($statistic) {
-		case "adjustedclose":			
+		case "adjusted close":	
+			$statistic = "adjustedClose";
 			break;
 		case "open":			
 			break;
@@ -106,31 +112,65 @@ function getStockData($ticker, $statistic, $resolution, $startDate, $endDate) {
 			break;
 	}
 	
-	$userDB = "JSCHEDEL";
-
 	
-	if($ticker >= 'SCI') {
+	
+	
+	$userDB = "JSCHEDEL";
+	if($ticker[$i] >= 'SCI') {
 		$userDB = "CHRISTIANMOSEY";
 		
-	} else if($ticker >= 'IBCP') {
+	} else if($ticker[$i] >= 'IBCP') {
 		$userDB = "\"C.ONOH\"";
 		
 	}
 	
 
+
+
+
+
 	if($startDate == NULL) {
 		
-		$dateStatement = oci_parse($conn, "SELECT reportDate FROM {$userDB}.stockReport WHERE stockTicker = '{$ticker}' ORDER BY reportDate ASC FETCH FIRST 1 ROW ONLY");
+		$dateStatement = oci_parse($conn, "SELECT reportDate FROM {$userDB}.stockReport WHERE stockTicker = '{$ticker[0]}' ORDER BY reportDate ASC FETCH FIRST 1 ROW ONLY");
 		
 		oci_execute($dateStatement);
-		
+
 		$startDate = oci_fetch_assoc($dateStatement)['REPORTDATE'];
+		
+		for ($j=1, $len=count($ticker); $j<$len; $j++) {
+			
+			$userDB = "JSCHEDEL";
+
+	
+			if($ticker[$j] >= 'SCI') {
+				$userDB = "CHRISTIANMOSEY";
 				
+			} else if($ticker[$j] >= 'IBCP') {
+				$userDB = "\"C.ONOH\"";
+				
+			}
+						
+			$dateStatement2 = oci_parse($conn, "SELECT reportDate FROM {$userDB}.stockReport WHERE stockTicker = '{$ticker[$j]}' ORDER BY reportDate ASC FETCH FIRST 1 ROW ONLY");
+			
+			
+			oci_execute($dateStatement2);
+
+			
+			$temp = oci_fetch_assoc($dateStatement2)['REPORTDATE'];
+			
+			if($temp > $startDate) {
+				
+				$startDate = $temp;
+				
+			}
+			
+		}
+
 		oci_free_statement($dateStatement);
 		
 	} else {
 		
-		$dateStatement = oci_parse($conn, "SELECT reportDate, ABS(reportDate - to_date('{$startDate}', 'YYYY-MM-DD')) AS dateDiff FROM {$userDB}.stockReport WHERE stockTicker = '{$ticker}' ORDER BY dateDiff ASC FETCH FIRST 1 ROWS ONLY");
+		$dateStatement = oci_parse($conn, "SELECT reportDate, ABS(reportDate - to_date('{$startDate}', 'YYYY-MM-DD')) AS dateDiff FROM {$userDB}.stockReport WHERE stockTicker = '{$ticker[$i]}' ORDER BY dateDiff ASC FETCH FIRST 1 ROWS ONLY");
 		
 		oci_execute($dateStatement);
 		
@@ -140,19 +180,64 @@ function getStockData($ticker, $statistic, $resolution, $startDate, $endDate) {
 		
 	}
 	
+	
+	
+	
+	
 	if($endDate == NULL) {
 		
-		$dateStatement2 = oci_parse($conn, "SELECT reportDate FROM {$userDB}.stockReport WHERE stockTicker = '{$ticker}' ORDER BY reportDate DESC FETCH FIRST 1 ROW ONLY");
 		
-		oci_execute($dateStatement2);
+		$userDB = "JSCHEDEL";
+		if($ticker[0] >= 'SCI') {
+		$userDB = "CHRISTIANMOSEY";
+		
+		} else if($ticker[0] >= 'IBCP') {
+			$userDB = "\"C.ONOH\"";
 			
-		$endDate = oci_fetch_assoc($dateStatement2)['REPORTDATE'];
+		}
+		
+		$dateStatement3 = oci_parse($conn, "SELECT reportDate FROM {$userDB}.stockReport WHERE stockTicker = '{$ticker[0]}' ORDER BY reportDate DESC FETCH FIRST 1 ROW ONLY");
+		
+		oci_execute($dateStatement3);
+
+		$endDate = oci_fetch_assoc($dateStatement3)['REPORTDATE'];
+		
+		for ($j=1, $len=count($ticker); $j<$len; $j++) {
+			
+			$userDB = "JSCHEDEL";
+			if($ticker[$j] >= 'SCI') {
+				$userDB = "CHRISTIANMOSEY";
 				
-		oci_free_statement($dateStatement2);
+			} else if($ticker[$j] >= 'IBCP') {
+				$userDB = "\"C.ONOH\"";
+				
+			}
+						
+			$dateStatement4 = oci_parse($conn, "SELECT reportDate FROM {$userDB}.stockReport WHERE stockTicker = '{$ticker[$j]}' ORDER BY reportDate DESC FETCH FIRST 1 ROW ONLY");
+			
+			
+			oci_execute($dateStatement4);
+
+			
+			$temp = oci_fetch_assoc($dateStatement4)['REPORTDATE'];
+			
+			if($temp < $endDate) {
+				
+				$endDate = $temp;
+				
+			}
+			
+		}
+
+		oci_free_statement($dateStatement3);
+		
+		
+		
+		
 		
 	} else {
 		
-		$dateStatement = oci_parse($conn, "SELECT reportDate, ABS(reportDate - to_date('{$endDate}', 'YYYY-MM-DD')) AS dateDiff FROM {$userDB}.stockReport WHERE stockTicker = '{$ticker}' ORDER BY dateDiff ASC FETCH FIRST 1 ROWS ONLY");
+		$dateStatement = oci_parse($conn, "SELECT reportDate, ABS(reportDate - to_date('{$endDate}', 'YYYY-MM-DD')) AS dateDiff FROM {$userDB}.stockReport WHERE stockTicker = '{$ticker[$i]}' ORDER BY dateDiff ASC FETCH FIRST 1 ROWS ONLY");
 		
 		oci_execute($dateStatement);
 		
@@ -162,7 +247,22 @@ function getStockData($ticker, $statistic, $resolution, $startDate, $endDate) {
 		
 	}
 	
-	$IDstatement1 = oci_parse($conn, "SELECT id FROM {$userDB}.stockReport WHERE stockTicker = '{$ticker}' AND reportDate = '{$startDate}'");
+	
+	$userDB = "JSCHEDEL";
+	if($ticker[$i] >= 'SCI') {
+		$userDB = "CHRISTIANMOSEY";
+		
+	} else if($ticker[$i] >= 'IBCP') {
+		$userDB = "\"C.ONOH\"";
+		
+	}
+	
+	
+	//exit($endDate);
+	
+	
+	
+	$IDstatement1 = oci_parse($conn, "SELECT id FROM {$userDB}.stockReport WHERE stockTicker = '{$ticker[$i]}' AND reportDate = '{$startDate}'");
 	
 	oci_execute($IDstatement1);
 	
@@ -171,7 +271,7 @@ function getStockData($ticker, $statistic, $resolution, $startDate, $endDate) {
 	oci_free_statement($IDstatement1);
 	
 	
-	$IDstatement2 = oci_parse($conn, "SELECT id FROM {$userDB}.stockReport WHERE stockTicker = '{$ticker}' AND reportDate = '{$endDate}'");
+	$IDstatement2 = oci_parse($conn, "SELECT id FROM {$userDB}.stockReport WHERE stockTicker = '{$ticker[$i]}' AND reportDate = '{$endDate}'");
 	
 	oci_execute($IDstatement2);
 	
@@ -195,25 +295,418 @@ function getStockData($ticker, $statistic, $resolution, $startDate, $endDate) {
 	$modResolution = $countDiff/$resolution;
 	
 	
-	$statement = oci_parse($conn, "SELECT reportDate, {$statistic} FROM {$userDB}.stockReport WHERE stockTicker = '{$ticker}' AND (((reportDate >= '{$startDate}') AND (reportDate <= '{$endDate}') AND (   MOD(ID, CAST({$modResolution} AS INT)) - MOD({$startID}, CAST({$modResolution} AS INT)) = '0'   )) OR reportDate = '{$endDate}') ORDER BY reportDate ASC");
+	$statement = oci_parse($conn, "SELECT reportDate, {$statistic} FROM {$userDB}.stockReport WHERE stockTicker = '{$ticker[$i]}' AND (((reportDate >= '{$startDate}') AND (reportDate <= '{$endDate}') AND (   MOD(ID, CAST({$modResolution} AS INT)) - MOD({$startID}, CAST({$modResolution} AS INT)) = '0'   )) OR reportDate = '{$endDate}') ORDER BY reportDate ASC");
 	
-	//exit("SELECT reportDate, {$statistic} FROM {$userDB}.stockReport WHERE stockTicker = '{$ticker}' AND (((reportDate >= '{$startDate}') AND (reportDate <= '{$endDate}') AND (   MOD(ID, CAST({$modResolution} AS INT)) - MOD({$startID}, CAST({$modResolution} AS INT)) = '0'   )) OR reportDate = '{$endDate}') ORDER BY reportDate ASC");
+	
 	
 	oci_execute($statement);
 	
-
+		
 	$response = array();
+	
+	
+	$a = 0;
 
-	while (($row = oci_fetch_object($statement))) {
-		$response[] = $row;
+
+	while (   ($row = oci_fetch_assoc($statement))) {
+		
+		
+		$response[$a]['Name'] = $row['REPORTDATE'];
+		
+		
+				
+		$response[$a][$ticker[0]] = $row[strtoupper($statistic)];
+		
+		
+		$a = $a + 1;
 	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	if(count($ticker) > 1) {
+		
+		$i = 1;
+		
+		$userDB2 = "JSCHEDEL";
+
+		
+		if($ticker[$i] >= 'SCI') {
+			$userDB2 = "CHRISTIANMOSEY";
+			
+		} else if($ticker[$i] >= 'IBCP') {
+			$userDB2 = "\"C.ONOH\"";
+			
+		}
+						
+		$IDstatement12 = oci_parse($conn, "SELECT id FROM {$userDB2}.stockReport WHERE stockTicker = '{$ticker[$i]}' AND reportDate = '{$startDate}'");
+		
+		oci_execute($IDstatement12);
+		
+		$startID2 = oci_fetch_assoc($IDstatement12)['ID'];
+				
+		oci_free_statement($IDstatement12);
+		
+		
+		$IDstatement22 = oci_parse($conn, "SELECT id FROM {$userDB2}.stockReport WHERE stockTicker = '{$ticker[$i]}' AND reportDate = '{$endDate}'");
+		
+		oci_execute($IDstatement22);
+		
+		$endID2 = oci_fetch_assoc($IDstatement22)['ID'];
+		
+		oci_free_statement($IDstatement22);
+		
+		//error("{$startID}   {$endID}");
+			
+		
+		$countDiff2 = (int)$endID2 - (int)$startID2;
+				
+		if($countDiff2 < $resolution) {
+			
+			$countDiff2 = $resolution;
+			
+		}
+		
+		$modResolution2 = $countDiff2/$resolution;
+		
+		
+		$statement2 = oci_parse($conn, "SELECT reportDate, {$statistic} FROM {$userDB2}.stockReport WHERE stockTicker = '{$ticker[$i]}' AND (((reportDate >= '{$startDate}') AND (reportDate <= '{$endDate}') AND (   MOD(ID, CAST({$modResolution2} AS INT)) - MOD({$startID}, CAST({$modResolution2} AS INT)) = '0'   )) OR reportDate = '{$endDate}') ORDER BY reportDate ASC");
+		
+		
+		oci_execute($statement2);
+				
+		
+		
+		$a = 0;
+
+		while ($row2 = oci_fetch_assoc($statement2)) {
+
+			$response[$a][$ticker[$i]] = $row2[strtoupper($statistic)];
+			
+			$a = $a + 1;
+		}
+		
+		oci_free_statement($statement2);
+		
+		unset($statement2);
+
+		
+		
+	}
+	
+	
+	if(count($ticker) > 2) {
+		
+		$i = 2;
+		
+		$userDB2 = "JSCHEDEL";
+
+		
+		if($ticker[$i] >= 'SCI') {
+			$userDB2 = "CHRISTIANMOSEY";
+			
+		} else if($ticker[$i] >= 'IBCP') {
+			$userDB2 = "\"C.ONOH\"";
+			
+		}
+		
+				
+		$IDstatement12 = oci_parse($conn, "SELECT id FROM {$userDB2}.stockReport WHERE stockTicker = '{$ticker[$i]}' AND reportDate = '{$startDate}'");
+		
+		oci_execute($IDstatement12);
+		
+		$startID2 = oci_fetch_assoc($IDstatement12)['ID'];
+				
+		oci_free_statement($IDstatement12);
+		
+		
+		$IDstatement22 = oci_parse($conn, "SELECT id FROM {$userDB2}.stockReport WHERE stockTicker = '{$ticker[$i]}' AND reportDate = '{$endDate}'");
+		
+		oci_execute($IDstatement22);
+		
+		$endID2 = oci_fetch_assoc($IDstatement22)['ID'];
+		
+		oci_free_statement($IDstatement22);
+		
+		//error("{$startID}   {$endID}");
+			
+		
+		$countDiff2 = (int)$endID2 - (int)$startID2;
+				
+		if($countDiff2 < $resolution) {
+			
+			$countDiff2 = $resolution;
+			
+		}
+		
+		$modResolution2 = $countDiff2/$resolution;
+		
+		
+		$statement2 = oci_parse($conn, "SELECT reportDate, {$statistic} FROM {$userDB2}.stockReport WHERE stockTicker = '{$ticker[$i]}' AND (((reportDate >= '{$startDate}') AND (reportDate <= '{$endDate}') AND (   MOD(ID, CAST({$modResolution2} AS INT)) - MOD({$startID}, CAST({$modResolution2} AS INT)) = '0'   )) OR reportDate = '{$endDate}') ORDER BY reportDate ASC");
+		
+		
+		oci_execute($statement2);
+				
+		
+		
+		$a = 0;
+
+		while ($row2 = oci_fetch_assoc($statement2)) {
+
+			$response[$a][$ticker[$i]] = $row2[strtoupper($statistic)];
+			
+			$a = $a + 1;
+		}
+		
+		oci_free_statement($statement2);
+		
+		unset($statement2);
+
+		
+		
+	}
+
+
+	
+	
+	
+	if(count($ticker) > 3) {
+		
+		$i = 3;
+		
+		$userDB2 = "JSCHEDEL";
+
+		
+		if($ticker[$i] >= 'SCI') {
+			$userDB2 = "CHRISTIANMOSEY";
+			
+		} else if($ticker[$i] >= 'IBCP') {
+			$userDB2 = "\"C.ONOH\"";
+			
+		}
+		
+				
+		$IDstatement12 = oci_parse($conn, "SELECT id FROM {$userDB2}.stockReport WHERE stockTicker = '{$ticker[$i]}' AND reportDate = '{$startDate}'");
+		
+		oci_execute($IDstatement12);
+		
+		$startID2 = oci_fetch_assoc($IDstatement12)['ID'];
+				
+		oci_free_statement($IDstatement12);
+		
+		
+		$IDstatement22 = oci_parse($conn, "SELECT id FROM {$userDB2}.stockReport WHERE stockTicker = '{$ticker[$i]}' AND reportDate = '{$endDate}'");
+		
+		oci_execute($IDstatement22);
+		
+		$endID2 = oci_fetch_assoc($IDstatement22)['ID'];
+		
+		oci_free_statement($IDstatement22);
+		
+		//error("{$startID}   {$endID}");
+			
+		
+		$countDiff2 = (int)$endID2 - (int)$startID2;
+				
+		if($countDiff2 < $resolution) {
+			
+			$countDiff2 = $resolution;
+			
+		}
+		
+		$modResolution2 = $countDiff2/$resolution;
+		
+		
+		$statement2 = oci_parse($conn, "SELECT reportDate, {$statistic} FROM {$userDB2}.stockReport WHERE stockTicker = '{$ticker[$i]}' AND (((reportDate >= '{$startDate}') AND (reportDate <= '{$endDate}') AND (   MOD(ID, CAST({$modResolution2} AS INT)) - MOD({$startID}, CAST({$modResolution2} AS INT)) = '0'   )) OR reportDate = '{$endDate}') ORDER BY reportDate ASC");
+		
+		
+		oci_execute($statement2);
+				
+		
+		
+		$a = 0;
+
+		while ($row2 = oci_fetch_assoc($statement2)) {
+
+			$response[$a][$ticker[$i]] = $row2[strtoupper($statistic)];
+			
+			$a = $a + 1;
+		}
+		
+		oci_free_statement($statement2);
+		
+		unset($statement2);
+
+		
+		
+	}
+	
+	
+	
+	
+	if(count($ticker) > 4) {
+		
+		$i = 4;
+		
+		$userDB2 = "JSCHEDEL";
+
+		
+		if($ticker[$i] >= 'SCI') {
+			$userDB2 = "CHRISTIANMOSEY";
+			
+		} else if($ticker[$i] >= 'IBCP') {
+			$userDB2 = "\"C.ONOH\"";
+			
+		}
+		
+				
+		$IDstatement12 = oci_parse($conn, "SELECT id FROM {$userDB2}.stockReport WHERE stockTicker = '{$ticker[$i]}' AND reportDate = '{$startDate}'");
+		
+		oci_execute($IDstatement12);
+		
+		$startID2 = oci_fetch_assoc($IDstatement12)['ID'];
+				
+		oci_free_statement($IDstatement12);
+		
+		
+		$IDstatement22 = oci_parse($conn, "SELECT id FROM {$userDB2}.stockReport WHERE stockTicker = '{$ticker[$i]}' AND reportDate = '{$endDate}'");
+		
+		oci_execute($IDstatement22);
+		
+		$endID2 = oci_fetch_assoc($IDstatement22)['ID'];
+		
+		oci_free_statement($IDstatement22);
+		
+		//error("{$startID}   {$endID}");
+			
+		
+		$countDiff2 = (int)$endID2 - (int)$startID2;
+				
+		if($countDiff2 < $resolution) {
+			
+			$countDiff2 = $resolution;
+			
+		}
+		
+		$modResolution2 = $countDiff2/$resolution;
+		
+		
+		$statement2 = oci_parse($conn, "SELECT reportDate, {$statistic} FROM {$userDB2}.stockReport WHERE stockTicker = '{$ticker[$i]}' AND (((reportDate >= '{$startDate}') AND (reportDate <= '{$endDate}') AND (   MOD(ID, CAST({$modResolution2} AS INT)) - MOD({$startID}, CAST({$modResolution2} AS INT)) = '0'   )) OR reportDate = '{$endDate}') ORDER BY reportDate ASC");
+		
+		
+		oci_execute($statement2);
+				
+		
+		
+		$a = 0;
+
+		while ($row2 = oci_fetch_assoc($statement2)) {
+
+			$response[$a][$ticker[$i]] = $row2[strtoupper($statistic)];
+			
+			$a = $a + 1;
+		}
+		
+		oci_free_statement($statement2);
+		
+		unset($statement2);
+
+		
+		
+	}
+	
+
+	if(count($ticker) > 5) {
+		
+		$i = 5;
+		
+		$userDB2 = "JSCHEDEL";
+
+		
+		if($ticker[$i] >= 'SCI') {
+			$userDB2 = "CHRISTIANMOSEY";
+			
+		} else if($ticker[$i] >= 'IBCP') {
+			$userDB2 = "\"C.ONOH\"";
+			
+		}
+		
+				
+		$IDstatement12 = oci_parse($conn, "SELECT id FROM {$userDB2}.stockReport WHERE stockTicker = '{$ticker[$i]}' AND reportDate = '{$startDate}'");
+		
+		oci_execute($IDstatement12);
+		
+		$startID2 = oci_fetch_assoc($IDstatement12)['ID'];
+				
+		oci_free_statement($IDstatement12);
+		
+		
+		$IDstatement22 = oci_parse($conn, "SELECT id FROM {$userDB2}.stockReport WHERE stockTicker = '{$ticker[$i]}' AND reportDate = '{$endDate}'");
+		
+		oci_execute($IDstatement22);
+		
+		$endID2 = oci_fetch_assoc($IDstatement22)['ID'];
+		
+		oci_free_statement($IDstatement22);
+		
+		//error("{$startID}   {$endID}");
+			
+		
+		$countDiff2 = (int)$endID2 - (int)$startID2;
+				
+		if($countDiff2 < $resolution) {
+			
+			$countDiff2 = $resolution;
+			
+		}
+		
+		$modResolution2 = $countDiff2/$resolution;
+		
+		
+		$statement2 = oci_parse($conn, "SELECT reportDate, {$statistic} FROM {$userDB2}.stockReport WHERE stockTicker = '{$ticker[$i]}' AND (((reportDate >= '{$startDate}') AND (reportDate <= '{$endDate}') AND (   MOD(ID, CAST({$modResolution2} AS INT)) - MOD({$startID}, CAST({$modResolution2} AS INT)) = '0'   )) OR reportDate = '{$endDate}') ORDER BY reportDate ASC");
+		
+		
+		oci_execute($statement2);
+				
+		
+		
+		$a = 0;
+
+		while ($row2 = oci_fetch_assoc($statement2)) {
+
+			$response[$a][$ticker[$i]] = $row2[strtoupper($statistic)];
+			
+			$a = $a + 1;
+		}
+		
+		oci_free_statement($statement2);
+		
+		unset($statement2);
+
+		
+		
+	}
+	
 		
 	oci_free_statement($statement);
+	//oci_close($conn);
+	
 	oci_close($conn);
+	
+	
+	
+	
+	
+	
 	
 	exit(json_encode($response));
 
-	
 }
 	
 	//Handles HTTP Requests for the file

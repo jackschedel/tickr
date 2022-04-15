83f59b5c3f97f52552b1b5d3b0b45d0b85649765
Chidiogo Onoh <chidi.onoh@gmail.com>
2022-04-14 21:36:55 -0400

    i added a months dropdown for the 'time' part of the sql queries and changed all the text size to 18 because i couldnt figure out how to make the menu boxes bigger

--- src/pages/landingPage.js
+++ src/pages/landingPage.js
@@ -4,6 +4,7 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import logo from "../dick.png";
 import StockSearch from "../components/StockSearch";
+import Time from "../components/Time";
 
 const LandingPage = () => {
     let navigate = useNavigate();
@@ -12,8 +13,9 @@ const LandingPage = () => {
             <img className={"logo"} src={logo} alt={"tickr logo"}/>
             <p style={{margin: "0 0 0"}}>Welcome to tickr</p>
             <StockSearch></StockSearch>
+            <Time></Time>
             <div align={"center"}>
-                <Button style= {{backgroundColor: "#5a8ab5", color: "#2a2b32", outlineWidth: 0}}  type="submit" onClick={() => navigate('/Stock')}>
+                <Button style= {{fontSize: 18, backgroundColor: "#5a8ab5", color: "#2a2b32", outlineWidth: 0}}  type="submit" onClick={() => navigate('/Stock')}>
                     Get Data
                 </Button>
             </div>

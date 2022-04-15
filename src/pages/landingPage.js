24d54efd3e3ecfe5940f3f7054e929821e144d5f
Chidiogo Onoh <chidi.onoh@gmail.com>
2022-04-15 03:57:03 -0400

    undid the commit had to check something

--- src/pages/landingPage.js
+++ src/pages/landingPage.js
@@ -5,12 +5,14 @@ import { useNavigate } from "react-router-dom";
 import logo from "../dick.png";
 import StockSearch from "../components/StockSearch";
 
+
 const LandingPage = () => {
     let navigate = useNavigate();
     return (<div className="App">
         <header className="App-header">
             <img className={"logo"} src={logo} alt={"tickr logo"}/>
             <p style={{margin: "0 0 0"}}>Welcome to tickr</p>
+            
             <StockSearch></StockSearch>
         </header>
     </div>)
